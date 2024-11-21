857429e11d6c7f744a21efeef594ae1d6e36572f
Phil Jirsa <phil.jirsa@microsoft.com>
2024-11-21 12:57:11 -0600

    planner prompt engineering

--- src/api/agents/planner.py
+++ src/api/agents/planner.py
@@ -1,21 +1,20 @@
-prompt = """For the following rules, make plans that can fetch the required data to validate the rules. For each plan, indicate \
+prompt = """For the following rule, make plans that can validate a patient meets the criteria defined by the rule. For each plan, indicate \
 which external tool together with tool input to retrieve evidence. You can store the evidence into a \
 variable #E that can be called by later tools. (Plan, #E1, Plan, #E2, Plan, ...)
 
 Tools can be one of the following:
-(1) Notes[input]: Worker that searches for patient visit notes. Useful when you need to confirm a diagnosis or treatment.
+(1) ChartNotes[input]: Worker that searches for patient visit notes. Useful when you need to confirm a diagnosis or treatment.
+(2) PatientData[input]: Worker that searches for specific patient data. Useful when looking up patient data.
 (2) LLM[input]: A pretrained LLM like yourself. Useful when you need to act with general
 world knowledge and common sense. Prioritize it when you are confident in solving the problem
 yourself. Input can be any instruction.
 
 For example,
-Task: Thomas Jones is being prescribed the medication {drug}
-Plan: Given Thomas worked x hours, translate the problem into algebraic expressions and solve
-with Wolfram Alpha. #E1 = WolframAlpha[Solve x + (2x − 10) + ((2x − 10) − 8) = 157]
-Plan: Find out the number of hours Thomas worked. #E2 = LLM[What is x, given #E1]
-Plan: Calculate the number of hours Rebecca worked. #E3 = Calculator[(2 ∗ #E2 − 10) − 8]
+Rule: The outside air temperature must be below 32 degrees Fahrenheit in order for it to be snowing.
+Plan: Look up the outside air temperature. #E1 = WeatherData[lookup outside air temperature]
 
 Begin! 
 Describe your plans with rich details. Each Plan should be followed by only one #E.
 
-Task: {task}"""
+Rule: {rule}"""
+
