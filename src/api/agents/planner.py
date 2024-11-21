9b0fdeb11341034118752efed7c1cfc3726c2533
Phil Jirsa <phil.jirsa@microsoft.com>
2024-11-21 11:24:37 -0600

    spike WIP

---
+++ src/api/agents/planner.py
@@ -0,0 +1,21 @@
+prompt = """For the following rules, make plans that can fetch the required data to validate the rules. For each plan, indicate \
+which external tool together with tool input to retrieve evidence. You can store the evidence into a \
+variable #E that can be called by later tools. (Plan, #E1, Plan, #E2, Plan, ...)
+
+Tools can be one of the following:
+(1) Notes[input]: Worker that searches for patient visit notes. Useful when you need to confirm a diagnosis or treatment.
+(2) LLM[input]: A pretrained LLM like yourself. Useful when you need to act with general
+world knowledge and common sense. Prioritize it when you are confident in solving the problem
+yourself. Input can be any instruction.
+
+For example,
+Task: Thomas Jones is being prescribed the medication {drug}
+Plan: Given Thomas worked x hours, translate the problem into algebraic expressions and solve
+with Wolfram Alpha. #E1 = WolframAlpha[Solve x + (2x − 10) + ((2x − 10) − 8) = 157]
+Plan: Find out the number of hours Thomas worked. #E2 = LLM[What is x, given #E1]
+Plan: Calculate the number of hours Rebecca worked. #E3 = Calculator[(2 ∗ #E2 − 10) − 8]
+
+Begin! 
+Describe your plans with rich details. Each Plan should be followed by only one #E.
+
+Task: {task}"""
