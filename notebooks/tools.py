b53d9579f0e938ac0b0b7d5b56dae8f42a2b6c0f
Phil Jirsa <phil.jirsa@microsoft.com>
2024-11-21 13:59:39 -0600

    test rewoo rule plans

---
+++ notebooks/tools.py
@@ -0,0 +1,18 @@
+from langchain_core.tools import tool
+
+@tool
+def get_patient_notes(patient_id: str):
+    """Get patient notes by patient ID."""
+    return {
+        "id": patient_id,
+        "notes": "Patient was diagnosed with Type 2 diabetes on 2023-01-09. Patient has an A1C level of 9.5%."
+    }
+    
+@tool
+def get_patient_data(patient_id: str):
+    """Get patient data by patient ID."""
+    return {
+        "id": patient_id,
+        "name": "John Doe",
+        "birthdate": "2010-01-05"
+    }
