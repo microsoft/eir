dfd294c78c3712b19138a19c7a567959316c63da
juantrujillo <juantrujillo@microsoft.com>
2024-11-22 14:43:19 -0500

    add endpoint to fetch drug qualification rules

--- src/api/main.py
+++ src/api/main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 #from .tools.patient_data import get_patient_data, get_patient_notes
 from .agents.planner import get_validation_plan
+from .tools.drug_rules import fetch_drug_rules
 
 import fastapi
 
@@ -20,3 +21,8 @@ async def generate_plan(request: plan_request):
 # @app.get("/get_plan")
 # async def get_plan(drug: str):
 #     return {"plan": f"Take 1 pill of {drug} every 4 hours"}
+
+@app.get("/get_rules")
+async def get_rules(drug:str):
+    result = fetch_drug_rules(drug)
+    return result
--- src/api/tools/drug_rules.py
+++ src/api/tools/drug_rules.py
@@ -1,8 +1,8 @@
 # write a function to return a list of drug qualification rules
 def fetch_drug_rules(drug: str):
-    return [
+    rules = [
         {"rule": "Patient must be 18 years or older", "logical": "AND"},
         {"rule": "Patient must have a confirmed diagnosis of Type 2 diabetes", "logical": "AND"},
         {"rule": "Patient must have an A1C level of 7.0% or higher", "logical": "AND"}
     ]
-        
+    return " ".join([f"{rule['rule']} {rule['logical']}" if i < len(rules) - 1 else rule['rule'] for i, rule in enumerate(rules)])
