33640c695b6b67ec5b09f5e348fde3f3d3bdf156
Awasthy <shiv_awasthy@optum.com>
2024-11-26 18:54:08 +0530

    Created tool to get lab results from ECDH and Chartnotes as per task 3220

---
+++ src/api/tools/get_lab_results/get_observation_data.py
@@ -0,0 +1,61 @@
+#importing libraries
+import os
+from dotenv import load_dotenv
+import datetime
+import json
+import requests
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
+from auth import get_access_token
+from langchain.agents import tool
+
+load_dotenv()
+
+chartnotes_api_url = os.getenv("CHARTNOTES_API_URL")
+ 
+
+def get_chartnotes(fileName: str, caseId: str) -> str:
+ 
+    """Calls chartnotes API to fetch diagnosis data for the patient."""
+   
+    token = get_access_token()
+   
+    payload={}
+   
+    headers = {
+    'x-upstream-env': 'dev',
+    'Authorization': f'Bearer {token}'
+    }
+   
+    url = f"{chartnotes_api_url}caseId={caseId}&fileName={fileName}&applicationName=PAS&access_token={token}"
+   
+    response = requests.request("GET", url, headers=headers, data=payload)
+ 
+    if response.status_code == 200:
+        chartnotes_res = response.json()
+        lab_data = chartnotes_res['MedicalNotes']['llmExtraction']['labResults']
+
+        return ({"labResults":lab_data}, 200)
+    else:
+        print('Failed to access the API', response.status_code,response.text)
+        return({"error": "Error in fetching chartnotes data."}, 500)
+ 
+@tool
+def get_observation_data(case_data):
+   
+    ECDH_data= case_data['ECDHDetails']['Observation']
+
+    if ECDH_data:
+        response = {"ECDH_details": ECDH_data}
+        return(response)
+    
+    else:
+        fileName = case_data["fileName"]
+        caseId = case_data["CaseId"]
+        response =get_chartnotes(fileName, caseId)
+        return(response)
+
+# with open('SampleRequestFormatFromPAS_Updated.json','r') as file:
+#     request_data = json.load(file)
+# ans = get_observation_data(request_data)
+# print(ans)
